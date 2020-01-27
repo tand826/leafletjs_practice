a2664b418d7d6496bbfc1a9445d0a71d9afd9b73
takumiando <takumiando@m.u-tokyo.ac.jp>
2020-01-27 12:57:38 +0900

    save

--- quick-start/app.py
+++ quick-start/app.py
@@ -1,39 +1,62 @@
+import os
+import sys
+from io import BytesIO
+from pathlib import Path
 from flask import Flask, jsonify, render_template, make_response
-import pyvips
-import time
+import openslide
+from openslide.deepzoom import DeepZoomGenerator
 
 app = Flask(__name__)
-sizes = [2**i for i in range(7, 17)]  # 128 ~ 65536
-z2size = {0: 10240, 1: 5120}
 
-path = "/Users/takumi/datasets/WSI/CMU-1.ndpi"
-app.slide = pyvips.Image.tiffload(path)
-max_zoom = int(app.slide.get("openslide.level-count"))
+# path = f"{os.environ.get('HOME')}/datasets/CMU-1.ndpi"
+path = sys.argv[1]
+tile_size = 256
+overlap = 0
 
-
-@app.route("/get_maxZoom")
-def get_maxZoom():
-    app.max_zoom = max_zoom
-    return jsonify({"maxZoom": max_zoom})
+app.slide = openslide.OpenSlide(path)
+app.dz = DeepZoomGenerator(app.slide,
+                           tile_size=tile_size,
+                           overlap=overlap)
 
 
-import numpy as np
-times = []
+@app.route("/get_properties")
+def get_properties():
+    z2slideWidth = {zoom: app.dz.level_dimensions[zoom][0] for zoom in range(0, app.dz.level_count-1)}
+    z2slideHeight = {zoom: app.dz.level_dimensions[zoom][1] for zoom in range(0, app.dz.level_count-1)}
+    z2patchWidth = {zoom: app.dz.get_tile_dimensions(zoom, (0, 0))[0] for zoom in range(0, app.dz.level_count-1)}
+    z2patchHeight = {zoom: app.dz.get_tile_dimensions(zoom, (0, 0))[1] for zoom in range(0, app.dz.level_count-1)}
+    z2tileCountLat = {zoom: app.dz.level_tiles[zoom][0] for zoom in range(0, app.dz.level_count-1)}
+    z2tileCountLng = {zoom: app.dz.level_tiles[zoom][1] for zoom in range(0, app.dz.level_count-1)}
+    properties = {"level_count": app.dz.level_count,
+                  "level_tiles": app.dz.level_tiles,
+                  "level_dimensions": app.dz.level_dimensions,
+                  "width": app.slide.dimensions[0],
+                  "height": app.slide.dimensions[1],
+                  "tile_size": tile_size,
+                  "overlap": overlap,
+                  "z2slideWidth": z2slideWidth,
+                  "z2slideHeight": z2slideHeight,
+                  "z2patchWidth": z2patchWidth,
+                  "z2patchHeight": z2patchHeight,
+                  "default_zoom": app.slide.level_count,
+                  "filename": Path(path).name,
+                  "z2tileCountLat": z2tileCountLat,
+                  "z2tileCountLng": z2tileCountLng
+                  }
+    return jsonify(properties)
+
+
 @app.route("/<loc>.png", methods=["POST", "GET"])
 def get_patch(loc):
     z, x, y = [int(i) for i in loc.split("_")]
-    patch_size = int(app.slide.get(f"openslide.level[{z}].tile-width")) * 2
-    print(patch_size)
-    scale = 256 / patch_size
-    x = patch_size * x
-    y = patch_size * y
-    patch = app.slide.crop(x, y, patch_size, patch_size).resize(scale)
-    now = time.time()
-    patch_png = patch.jpegsave_buffer(Q=95)
-    times.append(time.time()-now)
-    res = make_response(patch_png)
+    try:
+        patch = app.dz.get_tile(z, (x, y))
+    except ValueError:
+        return make_response()
+    buf = BytesIO()
+    patch.save(buf, "png", quality=95)
+    res = make_response(buf.getvalue())
     res.mimetype = f"image/png"
-    print(np.array(times).mean())
     return res
 
 
--- quick-start/static/index.js
+++ quick-start/static/index.js
@@ -1,37 +1,44 @@
-var slideWidth = 51200
-var slideHeight = 38144
-
-var zoom2tileSize = {0: 4096, 1: 2048}  // 続く
-var bounds = [-(slideHeight/zoom2tileSize[0])*256, (slideWidth/4096)*256]
-
 var mymap = L.map("mapid", {
-	zoom: 4,
 	center: [0, 0],
+	zoom: 9,
 	crs: L.CRS.Simple,
-	maxBounds: [[0, 0], bounds],
-	doubleClickZoom: "center"
 })
-// var zoomToSize = getSize(zoom)
 
-var maxZoom = getMaxZoom()
+function getProperties() {
 
-function getMaxZoom() {
-	fetch("/get_maxZoom", {
-		method: "GET",
-		mode: "cors"
-	}).then(response => {
-		if (response.ok) {
-			return response.json()
-		}
-	}).then(resJson => {
-		return Number(resJson["maxZoom"])
+	fetch("/get_properties", {
+	  method: "GET",
+	}).then(response => response.text()
+	).then(properties => {
+		var properties = JSON.parse(properties)
+		var fileName = properties["filename"]
+		var slideWidth = properties["width"]
+		var slideHeight = properties["height"]
+		var levelCount = properties["level_count"]
+		var z2slideWidth = properties["z2slideWidth"]
+		var z2slideHeight = properties["z2slideHeight"]
+		var z2patchWidth = properties["z2patchWidth"]
+		var z2patchHeight = properties["z2patchHeight"]
+		var z2tileCountLat = properties["z2tileCountLat"]
+		var z2tileCountLng = properties["z2tileCountLng"]
+		var tileSize = properties["tile_size"]
+		var overlap = properties["overlap"]
+		var defaultZoom = properties["default_zoom"] + 1
+
+		mymap.setView([-Math.floor(z2slideHeight[defaultZoom]/2/tileSize)+1,Math.floor(z2slideWidth[defaultZoom]/2/tileSize)-1], defaultZoom)
+		mymap.setMaxBounds([[0, 0], [-(slideHeight/z2patchHeight[defaultZoom]), (slideWidth/z2patchWidth[defaultZoom])]])
+		mymap.setMinZoom(defaultZoom)
+		mymap.setMaxZoom(levelCount-1)
+
+		L.tileLayer("/{z}_{x}_{y}.png", {
+			maxZoom: levelCount-1,
+			minZoom: defaultZoom,
+			tileSize: tileSize,
+			attribution: fileName
+			}
+		).addTo(mymap)
+
 	})
 }
 
-L.tileLayer("/{z}_{x}_{y}.png", {
-	maxZoom: 5,
-	minZoom: 0,
-	tileSize: 256,
-	attribution: "CMU-1.ndpi"
-	}
-).addTo(mymap)
+getProperties()
